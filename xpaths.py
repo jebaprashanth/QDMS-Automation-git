c7f759b0f67fe6817166defb2fe53f287e49bd71
jebaprashanth <100683851+jebaprashanth@users.noreply.github.com>
2022-12-14 10:32:07 +0530

    22

---
+++ xpaths.py
@@ -0,0 +1,26 @@
+# X_Paths - LOgin page
+
+login_username_xpath = '//*[@id="usernameOrEmail"]'
+login_password_xpath = '//*[@id="password"]'
+login_button_xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[1]/form/div/div[3]/div/div/span/button'
+
+notifications_close_button = '/html/body/div[2]/div/div/div/a/span/span/svg'
+
+# X_Paths - main page
+
+navigate_master_module_xpath = '//*[@id="root"]/div/section/section/main/div/div/div/div[1]/a/div/div'
+
+# X_Paths - master module page
+
+navigate_top_equipment_xpath = '//*[@id="root"]/div/section/section/header/ul/li[2]'
+navigate_left_equipment_xpath = '//*[@id="root"]/div/section/section/main/div/div/div[1]/div/div[1]'
+
+add_equipment_button_xpath = '//*[@id="root"]/div/section/section/main/div/div/div[2]/div/div/div/div/div[1]/div/div[3]/button'
+
+add_equipemnt_equipmentName_xpath = '//*[@id="equipment_name"]'
+add_equipment_equipmentType_xpath = '//*[@id="type"]'
+add_equipment_description_xpath = '//*[@id="equipment_description"]'
+
+add_equipment_saveButton_xpath = '/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[2]'
+add_equipment_cancelButton_xpath = '/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[1]'
+
